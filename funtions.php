2dfbedea576f73bf3ae0a048886dc4f02818aa3a
Josue Muñoz Avila <sjosue5082002@gmail.com>
2021-06-30 18:19:33 -0500

    Obteniendo los articulos de la Base de Datos

--- funtions.php
+++ funtions.php
@@ -3,7 +3,7 @@
 function conexion($bd_config){
 
     try {
-        $conexion = new PDO ('mysql:host=localhost;dbname=blog_practicaaw', 'root', '');
+        $conexion = new PDO('mysql:host=localhost;dbname='.$bd_config['basedatos'], $bd_config['usuario'], $bd_config['pass']);
         return $conexion;
     } catch (PDOException $e) {
         return false;
@@ -15,5 +15,16 @@ function limpiarDatos($datos){
     $datos = stripslashes($datos);
     $datos = htmlspecialchars($datos);
     return $datos;
+}
+
+function pagina_actual(){
+    return isset($_GET['p']) ? (int)$_GET['p'] : 1;
+}
+
+function obtener_post($post_por_pagina, $conexion){
+    $inicio = (pagina_actual() > 1) ? pagina_actual() * $post_por_pagina - $post_por_pagina : 0;
+    $sentencia = $conexion->prepare("SELECT SQL_CALC_FOUND_ROWS * FROM articulos LIMIT $inicio, $post_por_pagina");
+    $sentencia->execute();
+    return $sentencia->fetchAll();
 }
 ?>
