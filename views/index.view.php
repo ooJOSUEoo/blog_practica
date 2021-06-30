516aeaee03b3543df15bbb5fcfd55fc41cbb5922
Josue Muñoz Avila <sjosue5082002@gmail.com>
2021-06-30 18:30:55 -0500

    Cargando los articulos en la pagina principal

--- views/index.view.php
+++ views/index.view.php
@@ -1,32 +1,21 @@
 <?php require'header.php' ?>
 
     <div class="contenedor">
-        <div class="post">
-            <article>
-                <h2 class="titulo"><a href="#">Titulo del articulo</a></h2>
-                <p class="fecha">30 de junio del 2021</p>
-                <div class="thumb">
-                    <a href="#">
-                        <img src="<?php echo RUTA; ?>/imagenes/1.jpeg" alt="">
-                    </a>
-                </div>
-                <p class="extracto">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, quam.</p>
-                <a href="#" class="continuar">Continuar leyendo</a>
-            </article>
-        </div>
-        <div class="post">
-            <article>
-                <h2 class="titulo"><a href="#">Titulo del articulo</a></h2>
-                <p class="fecha">30 de junio del 2021</p>
-                <div class="thumb">
-                    <a href="#">
-                        <img src="<?php echo RUTA; ?>/imagenes/1.jpeg" alt="">
-                    </a>
-                </div>
-                <p class="extracto">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, quam.</p>
-                <a href="#" class="continuar">Continuar leyendo</a>
-            </article>
-        </div>
+        <?php foreach($posts as $post): ?>
+            <div class="post">
+                <article>
+                    <h2 class="titulo"><a href="single.php?id=<?php echo $post['id'] ?>"><?php echo $post['titulo'] ?></a></h2>
+                    <p class="fecha"><?php echo $post['fecha'] ?></p>
+                    <div class="thumb">
+                        <a href="single.php?id=<?php echo $post['id'] ?>">
+                            <img src="<?php echo RUTA; ?>/imagenes/<?php echo $post['thumb'] ?>" alt="">
+                        </a>
+                    </div>
+                    <p class="extracto"><?php echo $post['extracto'] ?></p>
+                    <a href="single.php?id=<?php echo $post['id'] ?>" class="continuar">Continuar leyendo</a>
+                </article>
+            </div>
+        <?php endforeach; ?>
 
         <?php require'paginacion.php' ?>
     </div>
