0029eb92917f0df4c39320bbb83a63865d0ac7ce
Josue Muñoz Avila <sjosue5082002@gmail.com>
2021-07-01 12:35:52 -0500

    Creando la barra de busqueda

---
+++ buscar.php
@@ -0,0 +1,31 @@
+<?php
+
+require 'admin/config.php';
+require 'funtions.php';
+
+$conexion = conexion($bd_config);
+if (!$conexion) {
+    header('Location: error.php');
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET'  && !empty($_GET['busqueda'])) {
+    $busqueda = limpiarDatos($_GET['busqueda']);
+
+    $statement = $conexion->prepare(
+        'SELECT * FROM articulos WHERE titulo LIKE :busqueda or texto like :busqueda'
+    );
+    $statement->execute(array(':busqueda' => "%$busqueda%"));
+    $resultados = $statement->fetchAll();
+
+    if (empty($resultados)) {
+        $titulo = 'No se encontraron articulos con el reultado: ' . $busqueda;
+    }else {
+        $titulo = 'Resultados de la busqueda: ' . $busqueda;
+    }
+}else {
+    header('Location: ' . RUTA . './index.php');
+}
+
+require './views/buscar.view.php';
+
+?>
---
+++ views/buscar.view.php
@@ -0,0 +1,23 @@
+<?php require'header.php' ?>
+
+<div class="contenedor">
+    <?php foreach($resultados as $post): ?>
+    <div class="post">
+        <article>
+            <h2 class="titulo"><a href="single.php?id=<?php echo $post['id'] ?>"><?php echo $post['titulo'] ?></a></h2>
+            <p class="fecha"><?php echo fecha($post['fecha']); ?></p>
+            <div class="thumb">
+                <a href="single.php?id=<?php echo $post['id'] ?>">
+                    <img src="<?php echo RUTA; ?>/imagenes/<?php echo $post['thumb'] ?>" alt="">
+                </a>
+            </div>
+            <p class="extracto"><?php echo $post['extracto'] ?></p>
+            <a href="single.php?id=<?php echo $post['id'] ?>" class="continuar">Continuar leyendo</a>
+        </article>
+    </div>
+    <?php endforeach; ?>
+
+    <?php require'paginacion.php' ?>
+</div>
+
+<?php require'footer.php' ?>
